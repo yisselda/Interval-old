6e36afc8cf360806f0ef25745c8964bb4a0e348a
Yisselda <yisselda@defmethod.io>
2019-09-20 13:11:02 -0400

    First commit

---
+++ Interval/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Interval
+//
+//  Created by Yisselda Rhoc on 9/20/19.
+//  Copyright © 2019 Yisselda Rhoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
