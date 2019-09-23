50891cdfc915a81aed74f8af41b1197f48b3f511
IsaacACasanova <isaacacasanova@gmail.com>
2019-09-22 20:06:10 -0400

    client authorization in AppDelegate

--- Interval/AppDelegate.swift
+++ Interval/AppDelegate.swift
@@ -8,39 +8,112 @@
 
 import UIKit
 
+private let SpotifyClientID = "e4aa9f015b3743ec9d51813eaf04cccd"
+private let SpotifyRedirectURL = URL(string: "spotify-ios-quick-start://spotify-login-callback")!
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    // MARK: - Properties
+    
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    private lazy var configuration: SPTConfiguration = {
+        return SPTConfiguration(clientID: SpotifyClientID,
+                                redirectURL: SpotifyRedirectURL)
+    }()
+    
+    private lazy var sessionManager: SPTSessionManager = {
+        if let tokenSwapURL = URL(string: "http://localhost:1234/swap"),
+            let tokenRefreshURL = URL(string: "http://localhost:1234/refresh") {
+            self.configuration.tokenSwapURL = tokenSwapURL
+            self.configuration.tokenRefreshURL = tokenRefreshURL
+            self.configuration.playURI = ""
+        }
+        
+        return SPTSessionManager(configuration: self.configuration, delegate: self)
+    }()
+    
+    private lazy var appRemote: SPTAppRemote = {
+        let appRemote = SPTAppRemote(configuration: configuration, logLevel: .debug)
+        appRemote.delegate = self
+        appRemote.connectionParameters.accessToken = "BQBnC9iGlFhBXYD7YM6CkWUQH-_tcU3XdYUaAJy3MnMp9DmbxWP5F9c2dqinSvWedCLVbjvhvEzddD-KGgiRqRFlFWg8S-cgNRf0pi7uPg0Iyke3i0cFPD5roj-KYZ57qgngrYeXjpsZQbeDH0UPKnTq3OgnwaHU6d6YTwy2Yg"
+        return appRemote
+    }()
+    
+    // MARK: - Application Lifecycle Methods
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let requestedScopes: SPTScope = [.appRemoteControl]
+        self.sessionManager.initiateSession(with: requestedScopes, options: .default)
+        
         return true
     }
-
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        self.sessionManager.application(app, open: url, options: options)
+        
+        return true
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        if appRemote.isConnected {
+            appRemote.disconnect()
+        }
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        if let _ = self.appRemote.connectionParameters.accessToken {
+            self.appRemote.connect()
+        }
     }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+    
 }
 
+// MARK: - Session Delegate Methods
+
+extension AppDelegate: SPTSessionManagerDelegate {
+    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+        self.appRemote.connectionParameters.accessToken = session.accessToken
+        self.appRemote.connect()
+    }
+    
+    func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
+        debugPrint(error.localizedDescription)
+    }
+    
+    func sessionManager(manager: SPTSessionManager, didRenew session: SPTSession) {
+        debugPrint(session)
+    }
+}
+
+// MARK: - App Remote Delegate Methods
+
+extension AppDelegate: SPTAppRemoteDelegate {
+    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+        self.appRemote.playerAPI?.delegate = self
+        self.appRemote.playerAPI?.subscribe(toPlayerState: { (result, error) in
+            if let error = error {
+                debugPrint(error.localizedDescription)
+            }
+        })
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
+        debugPrint("Connection failed")
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
+        debugPrint("Connection disconnected")
+    }
+}
+
+// MARK: - Player State Delegate Methods
+
+extension AppDelegate: SPTAppRemotePlayerStateDelegate {
+    func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
+        debugPrint("Track name: %@", playerState.track.name)
+    }
+}
---
+++ Interval/Bridging Header/Interval-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Interval-Bridging-Header.h
+//  Interval
+//
+//  Created by Isaac  Casanova on 9/20/19.
+//  Copyright © 2019 Isaac  Casanova. All rights reserved.
+//
+
+#ifndef Interval_Bridging_Header_h
+#define Interval_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* Interval_Bridging_Header_h */
