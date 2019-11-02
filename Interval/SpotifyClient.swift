17c120897fa6f9bf115597c97c40af0d51bd4ecc
Yisselda <yisselda.rhoc@gmail.com>
2019-11-02 17:02:51 -0400

    Enqueue appRemote.connect on the main thread.

--- Interval/SpotifyClient.swift
+++ Interval/SpotifyClient.swift
@@ -1,80 +1,3 @@
-//  private let SpotifyClientID = "2ab7f90f49a741c9a384b014163fd95b"
-//  private let SpotifyRedirectURL = URL(string: "spotify-ios-quick-start://spotify-login-callback")!
-//
-//  private lazy var configuration: SPTConfiguration = {
-//    return SPTConfiguration(clientID: SpotifyClientID,
-//                            redirectURL: SpotifyRedirectURL)
-//  }()
-//
-//  private lazy var sessionManager: SPTSessionManager = {
-//    if let tokenSwapURL = URL(string: "https://interval-spotify-auth.herokuapp.com/api/token"),
-//      let tokenRefreshURL = URL(string: "https://interval-spotify-auth.herokuapp.com/api/refresh_token") {
-//      self.configuration.tokenSwapURL = tokenSwapURL
-//      self.configuration.tokenRefreshURL = tokenRefreshURL
-//      self.configuration.playURI = "spotify:track:6194J9U7xviKfo0ZSsnxIc"
-//    }
-//
-//    return SPTSessionManager(configuration: self.configuration, delegate: self)
-//  }()
-//
-//  private lazy var appRemote: SPTAppRemote = {
-//    let appRemote = SPTAppRemote(configuration: configuration, logLevel: .debug)
-//    appRemote.delegate = self
-//    return appRemote
-//  }()
-//
-//  init() {
-//  let requestedScopes: SPTScope = [.appRemoteControl]
-//  self.sessionManager.initiateSession(with: requestedScopes, options: .default)
-//  }
-
-//// MARK: - Session Delegate Methods
-//
-//extension AppDelegate: SPTSessionManagerDelegate {
-//  func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
-//    self.appRemote.connectionParameters.accessToken = session.accessToken
-//    self.appRemote.connect()
-//  }
-//
-//  func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
-//    debugPrint(error.localizedDescription)
-//  }
-//
-//  func sessionManager(manager: SPTSessionManager, didRenew session: SPTSession) {
-//    debugPrint(session)
-//  }
-//}
-//
-//// MARK: - App Remote Delegate Methods
-//
-//extension AppDelegate: SPTAppRemoteDelegate {
-//  func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
-//    self.appRemote.playerAPI?.delegate = self
-//    self.appRemote.playerAPI?.subscribe(toPlayerState: { (result, error) in
-//      if let error = error {
-//        debugPrint(error.localizedDescription)
-//      }
-//    })
-//  }
-//
-//  func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
-//    debugPrint("Connection failed")
-//  }
-//
-//  func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
-//    debugPrint("Connection disconnected")
-//  }
-//}
-//
-//// MARK: - Player State Delegate Methods
-//
-//extension AppDelegate: SPTAppRemotePlayerStateDelegate {
-//  func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
-//    debugPrint("Track name: %@", playerState.track.name)
-//  }
-//}
-
-
 private struct Constants {
     static let SpotifyClientID = "2ab7f90f49a741c9a384b014163fd95b"
     static let SpotifyRedirectURL = URL(string: "spotify-ios-quick-start://spotify-login-callback")
@@ -101,7 +24,7 @@ class SpotifyClient: NSObject, SpotifyClientType {
         let configuration =  SPTConfiguration(clientID: Constants.SpotifyClientID, redirectURL: url)
         configuration.tokenSwapURL = Constants.tokenSwapURL
         configuration.tokenRefreshURL = Constants.tokenRefreshURL
-        configuration.playURI = "spotify:track:6194J9U7xviKfo0ZSsnxIc"
+        configuration.playURI = ""
         return configuration
     }()
     
@@ -159,8 +82,10 @@ extension SpotifyClient: SPTAppRemoteDelegate {
 extension SpotifyClient: SPTSessionManagerDelegate {
     func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
         debugPrint("didInitiate \(#function)")
-        self.appRemote.connectionParameters.accessToken = session.accessToken
-        self.appRemote.connect()
+        DispatchQueue.main.async {[weak self] in
+            self?.appRemote.connectionParameters.accessToken = session.accessToken
+            self?.appRemote.connect()
+        }
     }
     
     func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
